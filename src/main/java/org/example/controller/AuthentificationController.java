1f957e2fc006d6868fe09cbf70313bc3d1675e60
latir-sudo <diouf.francois-latir@ugb.edu.sn>
2026-01-10 16:08:07 +0000

    configuration du controller

--- src/main/java/org/example/controller/AuthentificationController.java
+++ src/main/java/org/example/controller/AuthentificationController.java
@@ -1,4 +1,88 @@
 package org.example.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.example.App;
+
 public class AuthentificationController {
+
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button cancelButton;
+    @FXML private Label errorLabel;
+
+    @FXML
+    public void initialize(){
+        // initialiser les composants
+
+        errorLabel.setVisible(false);
+
+        // configuration des actions
+
+        loginButton.setOnAction(event -> handleLogin());
+        cancelButton.setOnAction(event -> handleCancel());
+
+        // ceci permet de pouvoir se connecter avec le bouton enrée
+
+        usernameField.setOnAction(e-> handleLogin());
+        passwordField.setOnAction(e-> handleLogin());
+
+         // Focus sur le premier champ
+
+        usernameField.requestFocus();
+
+    }
+
+    @FXML
+    private void handleLogin() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if(username.isEmpty() || password.isEmpty()){
+             showErreur("Le mot de passe est obligatoire");
+             return ;
+
+        }
+
+        errorLabel.setVisible(false);
+        loginButton.setDisable(true); // je desactive le bouton pendant la vérification des informations
+
+        if (authenticate(username, password)) {
+
+            try{
+                App.loadUtilisateur();
+            }catch(Exception e){
+                showErreur("Erreur:"+ e.getMessage());
+                loginButton.setDisable(false);
+            }
+        }
+
+        else {
+            showErreur("email ou mot de passe incorrecte!");
+            loginButton.setDisable(false);
+        }
+    }
+
+    @FXML
+    private void handleCancel() {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
+
+
+
+
+    private void showErreur(String message){
+        errorLabel.setVisible(true);
+        errorLabel.setText(message);
+    }
 }
