a6d5a3680efa0929729592379a7ab7e99498881c
FaBadji <badji.fatou@ugb.edu.sn>
2026-01-12 11:50:36 +0000

    les modification de le branche de francois GestionLivre

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,7 +3,6 @@ package org.example;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-
 import javafx.stage.Stage;
 
 public class App extends Application {
