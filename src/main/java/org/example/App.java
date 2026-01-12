e18aab8ff76c6c5d8380b368f008b42b686172c6
latir-sudo <diouf.francois-latir@ugb.edu.sn>
2026-01-12 14:03:10 +0000

    accueil fonctionel avec adaption avec les packages org/example

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,43 @@
+package org.example;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // Charger le FXML
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/org/example/view/accueil.fxml")
+            );
+
+            Parent root = loader.load();
+
+            // Créer la scène
+            Scene scene = new Scene(root);
+
+            // Configurer la fenêtre
+            primaryStage.setTitle("Bibliothèque UGB - Gestion");
+            primaryStage.setScene(scene);
+            // primaryStage.setMinWidth(100);
+            // primaryStage.setMinHeight(700);
+
+            // Afficher
+            primaryStage.show();
+
+            System.out.println("✅ Application démarrée avec succès");
+
+        } catch (Exception e) {
+            System.err.println("❌ Erreur démarrage: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
